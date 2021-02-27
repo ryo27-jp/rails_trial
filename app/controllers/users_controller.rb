dd51a3270325b1172e16dfe3ea486d70d89b685d
ryo27-jp <calvee.20@gmail.com>
2021-02-27 18:22:10 +0900

    ユーザー登録・更新機能

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path, success: "登録しました"
+    else
+      flash.now[:danger] = '登録に失敗しました'
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to root_path, success: "更新しました"
+    else
+      flash.now[:danger] = '更新に失敗しました'
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :age, :password, :password_confirmation)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :email, uniqueness: true, presence: true
+  validates :password, presence: true, confirmation: true
+  validates :password_confirmation, presence: true
+
 end
