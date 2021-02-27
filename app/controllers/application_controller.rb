48f39465ec9d5e18661a338eda5353522ce21c9c
ryo27-jp <calvee.20@gmail.com>
2021-02-27 19:25:25 +0900

    ログインログアウト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   add_flash_types :success, :warning, :danger
+  helper_method :current_user
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
 end
