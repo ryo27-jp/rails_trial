183ae54b6a6f8c7449c67a4fd8ef9f246486bdfd
ryo27-jp <calvee.20@gmail.com>
2021-02-28 15:21:09 +0900

    バリーデーション忘れ

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,6 @@
 class Article < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true, length: { maximum: 20 }
+  validates :body, presence: true, length: { maximum: 100 }
 end
